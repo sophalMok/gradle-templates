2d15af97c0bbe99b3a94f2ca8d5ecdba7e1ec74d
Ryan Bascom <ryan.bascom@domo.com>
2015-04-21 11:58:26 -0600

    Removed promote task.

--- src/main/groovy/templates/ConnectorTemplatesPlugin.groovy
+++ src/main/groovy/templates/ConnectorTemplatesPlugin.groovy
@@ -13,7 +13,6 @@ class ConnectorTemplatesPlugin implements Plugin<Project> {
 
 	public static final String CREATE_CONNECTOR_TASK_NAME = 'createDomoConnector'
 	public static final String CONVERT_CONNECTOR_TASK_NAME = 'convertDomoConnector'
-	public static final String PROMOTE_CONNECTOR_TASK_NAME = 'promoteDomoConnector'
 	public static final String CREATE_CONNECTOR_TASK_DESCRIPTION = 'Creates a new Domo Connector project in a new directory named after your project.'
 	public static final String CONVERT_CONNECTOR_TASK_DESCRIPTION = 'Converts a Domo Connector ANT project to a Domo Connector GRADLE Project.'
 
